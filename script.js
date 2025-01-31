684c5613aa22675ddece869baee75ce01471346a
idontgiveaf <156725392+1dontgiveaf@users.noreply.github.com>
2025-01-30 21:39:20 -0500

    fix tons of bugs, redesign theme, added container in home, fix containers, unorignize everything, add hamburger button for small screens

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ const items = document.querySelectorAll('.item');
 items.forEach(item => {
     observer.observe(item);
 });
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    menu.classList.toggle('open');
+});
+
+window.addEventListener('resize', () => {
+	if (window.innerWidth >= 525) {
+		hamburger.classList.remove('open');
+		menu.classList.remove('open');
+	}
+});
